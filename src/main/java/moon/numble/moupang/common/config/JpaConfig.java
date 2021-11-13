1fcee0edc28b30a996437f2d4be2b06ff1b70186
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-13 17:34:39 +0900

    Add : 생성시간, 수정시간이 필요한 엔티티에 적용하기 위해 BaseTimeEntity 분리

---
+++ src/main/java/moon/numble/moupang/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package moon.numble.moupang.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
