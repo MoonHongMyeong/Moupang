e0f0beecfecd58c2168da51a2b6817142e9620f4
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-17 16:09:15 +0900

    Add : 로그인 체크를 위한 어노테이션 추가 [#4]

---
+++ src/main/java/moon/numble/moupang/common/annotation/LoginUser.java
@@ -0,0 +1,8 @@
+package moon.numble.moupang.common.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
