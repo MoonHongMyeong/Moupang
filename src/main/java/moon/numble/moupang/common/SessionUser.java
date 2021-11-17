506e6da3e412837d89b07c790e811bf78be429bb
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-17 16:20:57 +0900

    Add : 세션의 유저 정보를 직렬화하는 클래스 생성 [#4]

---
+++ src/main/java/moon/numble/moupang/common/SessionUser.java
@@ -0,0 +1,30 @@
+package moon.numble.moupang.common;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import moon.numble.moupang.user.domain.entity.User;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class SessionUser implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    private Long id;
+    private String email;
+    private String name;
+    private String role;
+
+    public SessionUser(User user){
+        this.id=user.getId();
+        this.email=user.getEmail();
+        this.name=user.getName();
+        this.role=user.getRoleKr();
+    }
+}
