5527a920d0bc7215a98d1ddf950170be167469c4
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-17 16:07:55 +0900

    Test : @AfterAll @BeforeAll 어노테이션을 사용하기 위해 @TestInstance 어노테이션 추가[#2]

--- src/test/java/moon/numble/moupang/domain/UserRepositoryTest.java
+++ src/test/java/moon/numble/moupang/domain/UserRepositoryTest.java
@@ -3,16 +3,14 @@ package moon.numble.moupang.domain;
 import moon.numble.moupang.user.domain.entity.Role;
 import moon.numble.moupang.user.domain.entity.User;
 import moon.numble.moupang.user.domain.repository.UserRepository;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
