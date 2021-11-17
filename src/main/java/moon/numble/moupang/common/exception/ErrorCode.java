7777f1c7f15b86c51ce5df11787a4b5f53c41cfc
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-17 16:24:09 +0900

    Add : 로그인 시 예외처리 정의 [#4]

--- src/main/java/moon/numble/moupang/common/exception/ErrorCode.java
+++ src/main/java/moon/numble/moupang/common/exception/ErrorCode.java
@@ -14,7 +14,9 @@ public enum ErrorCode {
     HANDLE_ACCESS_DENIED(403, "Handle Access Denied", "엑세스가 거부되었습니다."),
     INVALID_TYPE_VALUE(400, "Invalid Type Value", "잘못된 type 입니다."),
     //User
-    EMAIL_DUPLICATION(409, "Email Duplication", "중복된 이메일입니다.")
+    EMAIL_DUPLICATION(409, "Email Duplication", "중복된 이메일입니다."),
+    USER_NOT_FOUND(400, "User Not Found", "존재하지 않는 유저입니다."),
+    INVALID_PASSWORD(401, "Invalid Password", "비밀번호를 틀렸습니다.")
     ;
 
     private final int status;
