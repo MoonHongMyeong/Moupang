d7e9f67ce556d39eb3ca9a19fcf720ac8d786f6e
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-18 09:37:36 +0900

    Add : 로그인 체크를 위한 인터셉터 구현 [#5]

---
+++ src/main/java/moon/numble/moupang/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package moon.numble.moupang.common.interceptor;
+
+import moon.numble.moupang.common.SessionUser;
+import moon.numble.moupang.user.annotation.LoginRequired;
+import moon.numble.moupang.user.exception.UnauthorizedAccessException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private final String USER = "USER";
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        SessionUser user = (SessionUser) request.getAttribute(USER);
+
+        if(handlerMethod.hasMethodAnnotation(LoginRequired.class) && user == null){
+            throw new UnauthorizedAccessException(USER+" Unauthorized Access");
+        }
+
+        return true;
+    }
+
+}
