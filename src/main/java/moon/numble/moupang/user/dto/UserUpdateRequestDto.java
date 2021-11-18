83122df630e93e5cc845ccc1e4a4800ddbe23831
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-18 15:13:16 +0900

    Add : DTO 클래스에 validation 추가 [#5]

--- src/main/java/moon/numble/moupang/user/dto/UserUpdateRequestDto.java
+++ src/main/java/moon/numble/moupang/user/dto/UserUpdateRequestDto.java
@@ -3,12 +3,32 @@ package moon.numble.moupang.user.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 public class UserUpdateRequestDto {
+
+    @NotBlank
+    @Email(message = "유효하지 않은 이메일 형식입니다.",
+            regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
     private String email;
+
+    @NotBlank
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
     private String oldPassword;
+
+    @NotBlank
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
     private String newPassword;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String phone;
 
     @Builder
