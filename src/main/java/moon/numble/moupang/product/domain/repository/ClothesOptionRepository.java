950ec97d2036f50642eedf86e5fe77a0eed62597
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-23 17:00:35 +0900

    Add : 누락된 옷 상품 옵션 리포지토리 추가 [#12]

---
+++ src/main/java/moon/numble/moupang/product/domain/repository/ClothesOptionRepository.java
@@ -0,0 +1,7 @@
+package moon.numble.moupang.product.domain.repository;
+
+import moon.numble.moupang.product.domain.entity.ClothesOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClothesOptionRepository extends JpaRepository<ClothesOption, Long> {
+}
