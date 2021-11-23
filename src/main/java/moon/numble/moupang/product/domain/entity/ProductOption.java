494968217c10e3bd477534f7e2f46c951252ad9b
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-23 16:03:37 +0900

    Add : ProductOption 엔티티 생성 [#12]

---
+++ src/main/java/moon/numble/moupang/product/domain/entity/ProductOption.java
@@ -0,0 +1,41 @@
+package moon.numble.moupang.product.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import moon.numble.moupang.common.BaseTimeEntity;
+import moon.numble.moupang.product.dto.ProductOptionUpdateRequestDto;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ProductOption extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_option_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column
+    private String title;
+
+    @Column
+    private Double price;
+
+    @Builder
+    public ProductOption(Product product, String title, Double price) {
+        this.product = product;
+        this.title = title;
+        this.price = price;
+    }
+
+    public void update(ProductOptionUpdateRequestDto dto){
+        this.title=dto.getTitle();
+        this.price=dto.getPrice();
+    }
+}
