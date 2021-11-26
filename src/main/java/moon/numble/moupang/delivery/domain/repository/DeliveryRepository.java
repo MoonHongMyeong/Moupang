5def3f0c59176f9971a786248ec53c3f64704c9f
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-26 13:39:40 +0900

    Add : 배송 조회 기능 추가 [#23]

--- src/main/java/moon/numble/moupang/delivery/domain/repository/DeliveryRepository.java
+++ src/main/java/moon/numble/moupang/delivery/domain/repository/DeliveryRepository.java
@@ -3,5 +3,5 @@ package moon.numble.moupang.delivery.domain.repository;
 import moon.numble.moupang.delivery.domain.entity.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+public interface DeliveryRepository extends JpaRepository<Delivery, Long>, DeliveryCustomRepository {
 }
