4e729e4d571efd309415672bd3f7a587acec14b2
MoonHongMyeong <moonhongmyeong@gmail.com>
2021-11-27 20:15:51 +0900

    Add : 다른 디렉토리에서 사용할 수 있도록 리소스핸들러 오버라이딩 [#27]

--- src/main/java/moon/numble/moupang/common/config/WebConfig.java
+++ src/main/java/moon/numble/moupang/common/config/WebConfig.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import moon.numble.moupang.common.resolver.LoginUserArgumentResolver;
 import moon.numble.moupang.common.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Configuration
@@ -23,7 +26,16 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(loginInterceptor);
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/api/v1")
+                .excludePathPatterns("/");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/templates/", "file:src/main/resources/static/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
     }
 
 }
